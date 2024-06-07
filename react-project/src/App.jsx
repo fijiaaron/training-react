2708638482f0eb5a0b0279d6c6b277f4a77d09ad
Aaron Evans <aarone@one-shore.com>
2024-06-06 21:52:28 -0600

    adding form elements with custom styling

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -6,14 +6,37 @@ function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<>
-		<h1> React App </h1>
-		<div className="card">
-			<button onClick={() => setCount((count) => count + 1)}>
-			count is {count}
-			</button>
+		<div id="app">
+			<h1> Knightbase </h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					click!
+				</button>
+				<span style={{display: "inline-block", paddingLeft: "1em"}}>
+					counter: {count}
+				</span>
+
+				<form>
+					<label htmlFor="name">Name</label>
+					<input name="name" id="name" placeholder="enter your name"></input>
+
+					<label htmlFor="color">Favorite Color</label>
+					<select name="color" id="color">
+						<option selected disabled>select a color</option>
+						<option>red</option>
+						<option>yellow</option>
+						<option>green</option>
+						<option>blue</option>
+					</select>
+
+					<div>
+					<input name="subscribe" id="subscribe" type="checkbox"></input>
+					Subscribe to our newsletter
+					</div>
+
+				</form>
+			</div>
 		</div>
-		</>
 	)
 }
 
